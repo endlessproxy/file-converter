c39dd8263fb52321fa350ac12c2bb9d8e69e1be2
Andre S. <andre.simoesdutra23@gmail.com>
2024-10-04 02:30:15 -0300

    1.0.1

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ConverterArquivos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConvertFile();
+    }
+
+    static void ConvertFile()
+    {
+        Console.Clear();
+
+        string fileName = GetFileName("Digite o nome do arquivo: ");
+        string fileExtension = GetFileExtension("Digite a extensão do arquivo (ex: jpg, png, gif, js, html): ");
+        string filePath = Path.Combine(".", "Arquivos", fileName + fileExtension);
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Arquivo não encontrado!");
+            return;
+        }
+
+        Console.Clear();
+
+        var newFileExtension = GetFileExtension("Deseja converter para qual extensão? (ex: jpg, png, gif, js, html): ");
+        string newFilePath = Path.Combine(".", "Arquivos", fileName + newFileExtension);
+
+        if (File.Exists(newFilePath))
+        {
+            Console.WriteLine("Erro: Esse arquivo já existe!");
+            return;
+        }
+
+        TryConvertFile(filePath, newFilePath);
+    }
+
+    static string GetFileName(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine();
+    }
+
+    static string GetFileExtension(string message)
+    {
+        Console.Write(message);
+        string extension = Console.ReadLine().ToLower();
+        return extension.StartsWith(".") ? extension : "." + extension;
+    }
+
+    static void TryConvertFile(string sourceFile, string newFile)
+    {
+        try
+        {
+            File.Move(sourceFile, newFile);
+            Console.Clear();
+            Console.WriteLine($"Arquivo convertido com sucesso: {newFile}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao converter: {ex.Message}");
+        }
+    }
+}
